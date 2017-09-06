4e0c7d2aeed1f0bd8da7d9acf404a26d6d4469eb
nlimpid <jshuangzl@gmail.com>
2017-09-06 21:38:38 +0800

    test rancher

--- handler/home.go
+++ handler/home.go
@@ -2,6 +2,7 @@ package handler
 
 import "net/http"
 
+// GetHome test for the api
 func GetHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello rss and power by docker"))
+	w.Write([]byte("hello rss and power by docker!"))
 }
